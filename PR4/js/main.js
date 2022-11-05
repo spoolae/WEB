cde2a39d83ffcd1fbc73f27d1de32eae749c6008
Spoolae <ohotnic147feed@gmail.com>
2022-11-05 18:58:45 +0100

    PR4-5

--- PR4/js/main.js
+++ PR4/js/main.js
@@ -1,98 +1,85 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+new WOW().init();
 
-cartButton.addEventListener('click' , function(event) {
-    modal.classList.add("is-open");
+const cartBtn = document.querySelector("#cart-button");
+const cartModal = document.querySelector("#cart-modal");
+const cartModalClose = document.querySelector("#cart-modal-close");
+const cardsWrapper = document.querySelector("#cards");
+
+const mocks = [
+  {
+    img: "./img/rollugol.jpg",
+    title: "Ролл уголь стандарт",
+    ingredients: "Рис, угорь, соус унаги, кунжут, водоросли нори.",
+    price: 250,
+  },
+  {
+    img: "./img/california.jpg",
+    title: "Калифорния лосось стандарт",
+    ingredients:
+      "Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.",
+    price: 250,
+  },
+  {
+    img: "./img/okinava.jpg",
+    title: "Окинава стандарт",
+    ingredients:
+      "Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...",
+    price: 250,
+  },
+  {
+    img: "./img/cezar.jpg",
+    title: "Цезарь маки xl",
+    ingredients:
+      "Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...",
+    price: 250,
+  },
+  {
+    img: "./img/yasaymaki.jpg",
+    title: "Ясай маки стандарт 185 г",
+    ingredients:
+      "Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг",
+    price: 250,
+  },
+  {
+    img: "./img/rollkrevetka.jpg",
+    title: "Ролл с креветкой стандарт",
+    ingredients:
+      "Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы",
+    price: 250,
+  },
+];
+
+mocks.forEach((mock, index) => {
+  cardsWrapper.insertAdjacentHTML(
+    "beforeend",
+    `
+        <div class="card wow fadeInRightBig" data-wow-duration="2s" data-wow-delay="${
+          0.2 * index
+        }s">
+            <img src="${mock.img}" alt="image" class="card-image">
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${mock.title}</h3>
+                </div>
+                <div class="card-info">
+                    <div class="ingredients">${mock.ingredients}</div>
+                </div>
+                <div class="card-buttons">
+                    <button class="button button-primary">
+                        <span class="button-card-text">В корзину</span>
+                        <img class="button-card-image" src="img/shopping-cart-white.svg" alt="cart">
+                    </button>
+                    <strong class="card-price-bold">${mock.price} ₽</strong>
+                </div>
+            </div>
+        </div>
+    `
+  );
 });
 
-close.addEventListener('click' , function(event) {
-    modal.classList.remove("is-open");
-});
+cartBtn.addEventListener("click", toggleModal);
+cartModalClose.addEventListener("click", toggleModal);
 
-// new WOW().init();
-
-const buttonAuth = document.querySelector('.button-auth');
-const modalAuth = document.querySelector('.modal-auth');
-const closeAuth = document.querySelector(".close-auth");
-const logInForm = document.querySelector('#logInForm');
-const loginInput = document.querySelector('#login');
-const userName = document.querySelector('.user-name');
-const buttonOut = document.querySelector('.button-out');
-
-let login = localStorage.getItem('gloDelivery');
-
-
-const modalProb = document.querySelector('.modal-prob');
-const closeProb = document.querySelector(".close-prob");
-
-function ProbToggleModal() {
-    modalProb.classList.toggle("is-open");
+function toggleModal() {
+  cartModal.classList.toggle("active");
 }
-
-
-function LogToggleModal() {
-    modalAuth.classList.toggle("is-open");
-}
-
-function autorized(){
-    console.log('Da');
-
-    function logOut() {
-        login = null;
-        localStorage.removeItem('gloDelivery');
-        buttonAuth.style.display = '';
-        userName.style.display = '';
-        buttonOut.style.display = '';
-        userName.textContent = login;
-        buttonOut.removeEventListener('click', logOut);
-
-        checkAuth();
-    }
-
-    userName.textContent = login;
-
-    buttonAuth.style.display = 'none';
-    userName.style.display = 'inline';
-    buttonOut.style.display = 'block';
-    buttonOut.addEventListener('click', logOut);
-}
-
-function notAutorized(){
-    console.log('Net');
-
-    function logIn(event){
-        event.preventDefault();
-        login = loginInput.value;
-        localStorage.setItem('gloDelivery', login);
-
-        if (login) {
-            LogToggleModal();
-            buttonAuth.removeEventListener('click', LogToggleModal);
-            closeAuth.removeEventListener('click', LogToggleModal);
-            logInForm.removeEventListener('submit', logIn);
-            logInForm.reset();
-
-            checkAuth();
-        } 
-        else {
-            ProbToggleModal();
-            closeProb.addEventListener('click', ProbToggleModal);
-        }
-    }
-
-    buttonAuth.addEventListener('click', LogToggleModal);
-    closeAuth.addEventListener('click', LogToggleModal);
-    logInForm.addEventListener('submit', logIn);
-
-}
-
-function checkAuth(){
-    if (login) {
-        autorized();
-    } else {
-        notAutorized();
-    }
-}
-
-checkAuth();
