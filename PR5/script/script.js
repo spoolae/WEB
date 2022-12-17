a2acc6b47e39c0925d642b04cedb68980247df67
Spoolae <ohotnic147feed@gmail.com>
2022-12-17 11:51:41 +0100

    PR5-4

--- PR5/script/script.js
+++ PR5/script/script.js
@@ -1,149 +1,143 @@
-const openModalButton = document.querySelector('#btnOpenModal');
-const modal = document.querySelector('#modalBlock');
-const closeModal = document.querySelector('#closeModal');
-const questionTitle = document.querySelector('#question');
-const formAnswers = document.querySelector('#formAnswers');
-const nextButton = document.querySelector('#next');
-const prevButton = document.querySelector('#prev');
-const sendButton = document.querySelector('#send');
+const btnOpenModal = document.querySelector("#btnOpenModal");
+const modalBlock = document.querySelector("#modalBlock");
+const closeModal = document.querySelector("#closeModal");
+const questionTitle = document.querySelector("#question");
+const formAnswers = document.querySelector("#formAnswers");
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+const sendButton = document.querySelector("#send");
+let questionData;
 
-let questions = [];
-let page = 0;
-let finalAnswers = [];
-let numberQuestion = null;
+const getData = () => {
+  setTimeout(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyD1loTPvTVq8sYV9w_0JVYhFYP0bbLe0O0",
+      authDomain: "burgerseit.firebaseapp.com",
+      databaseURL: "https://burgerseit-default-rtdb.firebaseio.com",
+      projectId: "burgerseit",
+      storageBucket: "burgerseit.appspot.com",
+      messagingSenderId: "835509823965",
+      appId: "1:835509823965:web:b86b3dfcab9c86008eee34",
+      measurementId: "G-X7KW5NNQX6",
+    };
 
-closeModal.addEventListener('click', onCloseModalClick);
-openModalButton.addEventListener('click', onOpenModalBtnClick);
-nextButton.addEventListener('click', onNextBtnClick);
-prevButton.addEventListener('click', onPrevBtnClick);
-sendButton.addEventListener('click', onSendBtnClick);
+    firebase.initializeApp(firebaseConfig);
+    firebase
+      .database()
+      .ref()
+      .child("questions")
+      .once("value")
+      .then((snap) => {
+        questionData = snap.val();
+        formAnswers.textContent = "";
+        playTest();
+      });
+  }, 1000);
+};
 
-async function getQuestions(url) {
-  const resolve = await fetch(url);
-  const data = await resolve.json();
-  return data;
+function renderQuestions(id) {
+  switch (id) {
+    case 0:
+      prevButton.classList.add("d-none");
+      questionTitle.textContent = questionData[id].question;
+      renderAnswers(id);
+      break;
+
+    case questionData.length:
+      questionTitle.textContent = "";
+      formAnswers.innerHTML = `
+      <div class = "form-group">
+        <label for="numberPhone">Enter your number</label>
+        <input type="phone" class="form-control" id="numberPhone">
+      </div>
+      `;
+      nextButton.classList.add("d-none");
+      prevButton.classList.add("d-none");
+      sendButton.classList.add("d-block");
+      break;
+
+    case questionData.length + 1:
+      sendButton.classList.remove("d-block");
+      prevButton.classList.add("d-none");
+      formAnswers.textContent = "Спасибо за ваши ответы";
+      setTimeout(() => {
+        modalBlock.classList.remove("d-block");
+        formAnswers.innerHTML = "";
+      }, 2000);
+      break;
+
+    default:
+      prevButton.classList.remove("d-none");
+      nextButton.classList.remove("d-none");
+      sendButton.classList.add("d-none");
+      questionTitle.textContent = questionData[id].question;
+      renderAnswers(id);
+      break;
+  }
 }
 
-getQuestions('./questions.json')
-  .then(data => {
-    questions = data.questions;
-  })
-  .catch(error => {
-    console.log(error);
+function renderAnswers(id) {
+  questionData[id].answers.forEach((answer) => {
+    const question = `
+                <div class="answers-item d-flex justify-content-center">
+                    <input type="${questionData[id].type}" id="${answer.title}" name="answer" class="d-none" value = "${answer.title}">
+                    <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src="${answer.url}" alt="burger">
+                    <span>${answer.title}</span>
+                    </label>
+                </div>  
+            `;
+    formAnswers.insertAdjacentHTML("beforeend", question);
   });
-
-function onOpenModalBtnClick() {
-  modal.classList.add('d-block');
-  playTest();
 }
 
-function onCloseModalClick() {
-  modal.classList.remove('d-block');
-}
-
-function playTest() {
-  formAnswers.innerHTML = '';
-  sendButton.classList.add('d-none');
-
-  if (page === 0) {
-    prevButton.classList.add('d-none');
-  }
-  if (page > 0) {
-    prevButton.classList.remove('d-none');
-  }
-  if (page === questions.length) {
-    nextButton.classList.add('d-none');
-    prevButton.classList.add('d-none');
-    sendButton.classList.remove('d-none');
-    formAnswers.innerHTML = `
-
-
-       <div class="form-group">
-       <label for="numberPhone">Введіть свій номер телефону</label>
-       <input type="tel" name="phone" required pattern="^\+\d{2}\(\d{3}\)\s\d{3}[-]\d{2}[-]\d{2}$">
-       </div>
-
-
-
-      `;
-     questionTitle.textContent = '';
-     page = 0;
-    return;
-  }
-  if (page < questions.length) {
-    nextButton.classList.remove('d-none');
-  }
-
-  const renderAnswers = (answersArr, type) => {
-    answersArr.forEach(answer => {
-      const answerItem = document.createElement('div');
-      answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center');
-
-      answerItem.innerHTML = `
-      <input type="${type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}" />
-         <label for="${answer.title}" class="d-flex flex-column justify-content-between">
-             <img class="answerImg" src="${answer.url}" alt="burger" />
-             <span>${answer.title}</span>
-         </label>
-      `;
-
-      formAnswers.appendChild(answerItem);
-    });
-  };
-
-  const renderQuestions = index => {
-    numberQuestion = index;
-    questionTitle.textContent = `${questions[index].question}`;
-
-    renderAnswers(questions[index].answers, questions[index].type);
-  };
-
-  renderQuestions(page);
-}
-
-function checkAnswer() {
-  const obj = {};
-
+function checkAnswers(questionId, obj) {
   const inputs = [...formAnswers.elements].filter(
-    element => element.checked || element.id === 'numberPhone'
+    (input) => input.checked || input.id === "numberPhone"
   );
+  console.log(inputs);
   inputs.forEach((input, index) => {
-    if (input.id === 'numberPhone') {
-      obj['Номер телефону'] = input.value;
-    } else {
-      obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+    if (questionId >= 0 && questionId < questionData.length) {
+      obj[`${index}_${questionData[questionId].question}`] = input.value;
+    }
+    if (questionId === questionData.length) {
+      obj["Номер телефона"] = input.value;
     }
   });
-  finalAnswers.push(obj);
+}
+function playTest() {
+  let questionId = 0;
+  const resultObj = {};
+  prevButton.classList.remove("d-none");
+  nextButton.classList.remove("d-none");
+  sendButton.classList.add("d-none");
+  prevButton.addEventListener("click", () => {
+    questionId--;
+    formAnswers.innerHTML = "";
+    formAnswers.textContent = "";
+    renderQuestions(questionId);
+  });
+  nextButton.addEventListener("click", () => {
+    checkAnswers(questionId, resultObj);
+    questionId++;
+    formAnswers.innerHTML = "";
+    renderQuestions(questionId);
+  });
+  sendButton.addEventListener("click", () => {
+    checkAnswers(questionId, resultObj);
+    questionId++;
+    renderQuestions(questionId);
+    firebase.database().ref().child("contacts").push(resultObj);
+  });
+  renderQuestions(questionId);
 }
 
-function onNextBtnClick() {
-  checkAnswer();
-  page += 1;
-  playTest();
-}
+btnOpenModal.addEventListener("click", () => {
+  modalBlock.classList.add("d-block");
+  getData();
+});
 
-function onPrevBtnClick() {
-  page -= 1;
-  playTest();
-}
-
-function onSendBtnClick() {
-  checkAnswer();
-  formAnswers.textContent = 'Дякую за пройдений тест';
-  sendButton.classList.add('d-none');
-  setTimeout(() => {
-    modal.classList.remove('d-block');
-  }, 2000);
-
-  console.log(finalAnswers);
-  finalAnswers = [];
-}
-
-var inputsTel = document.querySelectorAll('input[type="tel"]');
-
-Inputmask({
-  "mask": "+38(999) 999-99-99",
-  showMaskOnHover: false
-}).mask(inputsTel);
-
+closeModal.addEventListener("click", () => {
+  modalBlock.classList.remove("d-block");
+  formAnswers.innerHTML = "";
+});
